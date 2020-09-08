b863a3827f23a887bc056eca90441fde0174fc2a
Aasa-Christian <aasachristian@yahoo.com>
2020-09-08 19:46:57 -0400

    created dashboard to hold all country data

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,13 +1,27 @@
-import React, { useEffect} from 'react';
-import axios from 'axios';
+import React, { useState ,useEffect} from 'react';
+import {NavLink} from "react-router-dom"
+import InfoCard from './InfoCard';
 
 const DashBoard = props => {
-    return(
 
-        <div key = {props.key}>
-            <p>{props.name}</p>
-            
+    return(
+        <div>
+            {props.cData.map(country => (
+                <div key={country.name}>
+
+                        <NavLink to = {`/location/${country.name}`}>
+                    <div >
+                        <InfoCard name={country.name} />
+                    </div>
+
+                </NavLink>
+
+                </div>
+
+                
+            ))}
         </div>
+        
         
     )
 }
