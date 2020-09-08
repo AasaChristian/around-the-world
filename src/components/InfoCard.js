8e5e69be5dadae8c22a814d7d6f58179708240c6
Aasa-Christian <aasachristian@yahoo.com>
2020-09-08 19:46:05 -0400

    created card to hold contry details

---
+++ src/components/InfoCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const InfoCard = props => {
+
+    return(
+        <div key = {props.key}>
+            <p>{props.name}</p>
+        </div>
+        
+        
+        
+    )
+}
+export default InfoCard
