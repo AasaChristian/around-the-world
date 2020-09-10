0389d1640018d31218f076d9e53749385295b130
Aasa-Christian <aasachristian@yahoo.com>
2020-09-10 16:49:59 -0400

    small updates

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
 import React, {useState ,useEffect} from 'react';
-import logo from './logo.svg';
+import {Route} from "react-router-dom";
+import axios from "axios"
 import './App.css';
-import axios from 'axios';
 import DashBoard from './components/dashboard';
+import Country from './components/Country';
 
 function App() {
   const [cData, setCData] = useState([])
 
   useEffect(() => {
-    axios
-    .get('https://restcountries-v1.p.rapidapi.com/all')
-    .then(res => {
-      console.log(res.data)
-      setCData(res.data)
-    })
-    .catch(error => {
-      console.log(error)
-    })
+    axios({
+      "method":"GET",
+      "url":"https://restcountries-v1.p.rapidapi.com/all",
+      "headers":{
+      "content-type":"application/octet-stream",
+      "x-rapidapi-host":"restcountries-v1.p.rapidapi.com",
+      "x-rapidapi-key":"d99e177f0cmsh5c3cc2147cd5808p1ad5dcjsnd3c8290db918",
+      "useQueryString":true
+      }
+      })
+      .then((response)=>{
+        console.log(response)
+        setCData(response.data)
+      })
+      .catch((error)=>{
+        console.log(error)
+      })
   },[])
 
-  console.log(cData, "cData")
   return (
     <div className="App">
-      {cData.map(countries => (
-        <DashBoard
-        name ={countries.name}
-        key = {countries.area}
-        />
-      ))}
+      <Route exact path="/"
+      render={props => <DashBoard {...props} cData={cData}/>}
+      />
+      <Route 
+      path ="/location/:name"
+      render={props => <Country {...props} cData={cData} />} />
     </div>
   );
 }
