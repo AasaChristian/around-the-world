b939034f6399fd1431c6d7d0a8ef36324c7cb6ab
Aasa-Christian <aasachristian@yahoo.com>
2020-09-10 16:48:01 -0400

    added RunFetch function to fix reloading page bug

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,17 +1,51 @@
 import React, { useState ,useEffect} from 'react';
 import InfoCard from './InfoCard'
+import Axios from 'axios';
 
 const Country = props => {
 
+    const [currentCountry, setCurrentCountry] = useState()
+    
     const country = props.cData.find(
         findName => findName.name === props.match.params.name
     )
+
+    console.log(props, "props")
+
+    console.log(currentCountry, "currentCountry")
+
+
+
+    const RunFetch = name => {
+        Axios({
+            "method":"GET",
+            "url":`https://restcountries-v1.p.rapidapi.com/name/${name}`,
+            "headers":{
+            "content-type":"application/octet-stream",
+            "x-rapidapi-host":"restcountries-v1.p.rapidapi.com",
+            "x-rapidapi-key":"d99e177f0cmsh5c3cc2147cd5808p1ad5dcjsnd3c8290db918",
+            "useQueryString":true
+            }
+            })
+            .then((response)=>{
+              console.log(response)
+              setCurrentCountry(response.name)
+            })
+            .catch((error)=>{
+              console.log(error)
+            })
+    }
+console.log((props.match.params.name).replaceAll(" ", ""), "props.history")
+    if (country === undefined){
+        RunFetch((props.match.params.name).replaceAll(" ", ""))
+    }
+
+console.log(currentCountry, "currentCountry")
+
     return(
         <div>
-            <InfoCard name={country.name} />
+            <InfoCard name={country !== undefined? country.name: currentCountry}/>
         </div>
-        
-        
     )
 }
 export default Country
